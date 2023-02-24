45eabea79c452af5e978e9217a44a214834e6e19
Hisgraceingrace <hisgraceingrace2000@yahoo.com>
2023-02-24 15:16:22 -0800

    prime factors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,15 +15,16 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
+			_putchar(',')
+		}
 			for (j = i; j < size; j++)
 			{
 					_putchar(' ');
-		}
+			}
 		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
-		}
 	}
-}
+}	
